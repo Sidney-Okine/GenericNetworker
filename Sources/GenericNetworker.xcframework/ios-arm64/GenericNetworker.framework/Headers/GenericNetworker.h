888c0912e72c0f65e314d7fe28182959cd44b5e1
sidney.okine <sidney.okine@hubtel.com>
2023-03-21 18:07:32 +0000

    Initial Commit

---
+++ Sources/GenericNetworker.xcframework/ios-arm64/GenericNetworker.framework/Headers/GenericNetworker.h
@@ -0,0 +1,18 @@
+//
+//  GenericNetworker.h
+//  GenericNetworker
+//
+//  Created by Sidney Okine on 21/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GenericNetworker.
+FOUNDATION_EXPORT double GenericNetworkerVersionNumber;
+
+//! Project version string for GenericNetworker.
+FOUNDATION_EXPORT const unsigned char GenericNetworkerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericNetworker/PublicHeader.h>
+
+
--- Sources/GenericNetworker/GenericNetworker.swift
+++
@@ -1,6 +0,0 @@
-public struct GenericNetworker {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
